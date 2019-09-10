2b30cf34fbe4c0dc0db779c2d27d68cd47c634eb
kazikkoduje <kazik@kazikkoduje.pl>
2019-09-10 17:00:17 +0200

    done

---
+++ Dzien_1/MemeGenerator/webpack.config.js
@@ -0,0 +1,70 @@
+//Konfiguracja Webpack
+var path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+
+module.exports = {
+  //devtool: 'cheap-eval-source-map',
+  watch: true,
+  mode: 'production',
+  entry: {
+    main: path.join(__dirname, 'src', 'index.js')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4500,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env', '@babel/preset-react'
+            ],
+            "plugins": [
+              [
+                "@babel/plugin-proposal-class-properties", {
+                  "loose": true
+                }
+              ]
+            ]
+          }
+        }
+      }, {
+        test: /\.(png|jpe?g|svg|gif|woff|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/'
+          }
+        }
+      }, {
+        test: /\.css/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      }, {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+     template: path.join(__dirname, 'src', 'index.html'),
+     hash: true
+   }),
+    new MiniCssExtractPlugin({filename: "[name].css", chunkFilename: "[id].css"})
+  ]
+}
