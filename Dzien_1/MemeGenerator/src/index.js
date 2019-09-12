fa782328a072aad1b1a217a9581fe98c4f040df4
kazikkoduje <kazik@kazikkoduje.pl>
2019-09-12 14:12:51 +0200

    done

--- Dzien_1/MemeGenerator/src/index.js
+++ Dzien_1/MemeGenerator/src/index.js
@@ -1,10 +1,79 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class MathTest extends React.Component {
-   render(){
-     return (
+class MemeGenerator extends React.Component {
 
+  state = {
+    memes: [],
+    choosenMeme: null
+  }
+
+  //username: MarcinKieruzel pass: kazik
+
+  handleClick(e, el){
+    this.setState({
+      choosenMeme: el
+    })
+  }
+
+
+
+   render(){
+
+     console.log(this.state.memes);
+
+     let memes = this.state.memes.map((el,ix) => {
+        return <div key={ix} style={{width: "25%", padding: "10px"}}>
+            <img onClick={(e) => this.handleClick(e, el)} style={{maxWidth: "100%"}} src={el.url} />
+        </div>
+     })
+     return (
+       <div style={{display: "flex", flexWrap: "wrap", margin: "0 auto", width: "100%", maxWidth: "1200px"}}>
+       {
+         this.state.choosenMeme &&
+         <div>
+         <figure>
+           ...Tutaj wgraj mema
+         </figure>
+          <figure>
+            <img src={this.state.choosenMeme.url} />
+            <figcaption>Wybrany meme</figcaption>
+          </figure>
+          <form>
+            <input name="text0" />
+            <input name="text1" />
+            <button>Get meme</button>
+          </form>
+         </div>
+       }
+
+
+          {memes}
+       </div>
      )
    }
+
+   componentDidMount() {
+      fetch('https://api.imgflip.com/get_memes')
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        console.log(res);
+        this.setState({memes: res.data.memes})
+      })
+      .catch(res => {
+        console.log(res);
+      })
+   }
+
  }
+
+ document.addEventListener("DOMContentLoaded", function(){
+
+    ReactDOM.render(
+      <MemeGenerator />,
+      document.querySelector("#app")
+    )
+
+  })
